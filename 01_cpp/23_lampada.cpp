fbd41b3e1f549629aed242283536d950445da668
Rubens Rabelo <rubensrabelo123@gmail.com>
2023-11-19 17:51:34 -0300

    Enviando o ex 23

---
+++ 01_cpp/23_lampada.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n=0, a=0, b=0;
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int t=0;
+
+        cin >> t;
+
+        if(t == 1){
+            a == 0? a = 1 : a = 0;
+        } else{
+            a == 0? a = 1 : a = 0;
+            b == 0? b = 1 : b = 0;
+        }
+    }
+
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
